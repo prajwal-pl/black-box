fdc0208f97ecd66dbb2bbec4deef7c175dd44446
Prajwal P L <prajwalpl096@gmail.com>
2026-06-20 22:29:59 +0530

    feat: implement case management controller and routes

---
+++ backend/controllers/cases.controller.ts
@@ -0,0 +1,11 @@
+import { type RequestHandler } from "express";
+
+export const getCases: RequestHandler = (req, res) => { }
+
+export const createCase: RequestHandler = (req, res) => { }
+
+export const getCaseById: RequestHandler = (req, res) => { }
+
+export const updateCase: RequestHandler = (req, res) => { }
+
+export const deleteCase: RequestHandler = (req, res) => { }
--- backend/routes/cases.ts
+++ backend/routes/cases.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import { createCase, deleteCase, getCaseById, getCases, updateCase } from '../controllers/cases.controller';
 
 const router = express.Router();
 
-
+router.get("/", getCases)
+router.post("/", createCase)
+router.get("/:id", getCaseById)
+router.put("/:id", updateCase)
+router.delete("/:id", deleteCase)
 
 export default router;
